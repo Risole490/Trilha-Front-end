7b2cc0fc745c8c5a35e77b883bc7d65a7c4ddb25
Leonardo <71907948+Risole490@users.noreply.github.com>
2022-07-14 19:33:28 -0300

    Add files via upload

--- JavaScript/Examples/array.js
+++ JavaScript/Examples/array.js
@@ -11,7 +11,7 @@ const hitchedSpaceships = [
 let morethan9 = hitchedSpaceships.filter(currentSpaceship => { return currentSpaceship[1] >= 9}).map(element => { return element[0]})
 // percorre o array hitchedSpaceships, acessa o primeiro subarray e filtra o elemento de index 1 que seja maior que 9 e retorna para a variável morethan9. Depois, retornados os resultados, o .map cria um array com apenas o nome da nave, e não todos os dados do subarray.
 
-let pendantPlatform = hitchedSpaceships.findIndex(currentSpaceship => { return currentSpaceship[2] == false}) + 1
+let pendantPlatform = hitchedSpaceships.findIndex(currentSpaceship => { return currentSpaceship[2] == false})+1
 // percorre o array hitchedSpaceships, acessa os subarrays até achar o primeiro que tiver o 2 index com o valor "false" e retorna o número do index do array.
 
 let upcasedSpaceships = hitchedSpaceships.map(currentSpaceship => {
@@ -20,4 +20,4 @@ let upcasedSpaceships = hitchedSpaceships.map(currentSpaceship => {
 })
 // percorre o array hitchedSpaceships, acessa os subarrays no index 0 e deixa toda a string em caps lock. Retorna para a variável upcased o valor em caps lock, e armazena no upcasedSpaceships para ir para a proxima subarray.
 
-alert("Naves com mais de 9 tripulantes: "+ morethan9 + "\nPlataforma com engate pendente: "+ pendantPlatform + "\nNaves: " + upcasedSpaceships)
+alert("Naves com mais de 9 tripulantes: "+ morethan9.join(", ") + "\nPlataforma com engate pendente: "+ pendantPlatform + "\nNaves: " + upcasedSpaceships.join(", "))
